aa51f735da02fd34e35c95f40e179562402bd361
Mariano Troncoso <petotronco@gmail.com>
2020-04-19 12:13:09 -0300

    seguimos con la preparacion para el deplyment

--- server/index.js
+++ server/index.js
@@ -1,20 +1,17 @@
  
 // importar express
 const express = require('express');
-
 // acceder al file system 
 const path = require('path');
-
 // para leer datos del formulario de testimoniales ??????
 const bodyParser = require('body-parser');
 
 // importar archivo con las rutas
 const routes = require('./routes');
-
 const configs = require('./config');
-
-// traigo la contexxion 
 const db = require('./config/database');
+
+require('dotenv').config({path: 'variables.env'})
 
 // pruebo la conexion
 // db.authenticate()
@@ -57,5 +54,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 // cargar rutas
 app.use('/', routes() )
 
-
-app.listen(3000);
+// puerto y host para la app 
+// como no sabemos que valores de puerto vamos a usar, lo dejamos libre para que 
+// eroku lo asigne
+const host = process.env.HOST || '0.0.0.0';
+// 0.0.0.0 es la direccion asignada por eroku por si falla algo
+const port = process.env.PORT || 3000;
+// app.listen(3000);
+app.listen(port, host, () => {
+   console.log('el servidor esta funcionando')
+} )
